47b12822e0a2c832747bccbf81adaada5d9b0c99
Jaya <dhanipatijayalakshmi@gmail.com>
2025-01-06 18:20:16 +0530

    development commit

---
+++ src/test/java/AutomationPractice/Shapes/ShapesInMaths.java
@@ -0,0 +1,19 @@
+package AutomationPractice.Shapes;
+
+import org.testng.annotations.Test;
+
+public class ShapesInMaths {
+	
+	@Test
+	public void TriangleShape()
+	{
+		System.out.println("Triangle is a shape with 3 angles");
+	}
+	
+	@Test
+	public void RectangleShape() {
+		
+		System.out.println("Rectangle is a shape with 4 angles");
+	}
+
+}
