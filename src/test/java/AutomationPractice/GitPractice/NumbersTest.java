256fae08e59d6452cb56c3e9a44edfe05a2a5b16
Jaya <dhanipatijayalakshmi@gmail.com>
2025-01-06 17:01:38 +0530

    second commit

--- src/test/java/AutomationPractice/GitPractice/NumbersTest.java
+++ src/test/java/AutomationPractice/GitPractice/NumbersTest.java
@@ -4,7 +4,12 @@ public class NumbersTest {
 
 	public void EvenNumbers() {
 		
-		System.out.println("2 is an Even Number	");
+		System.out.println("2 is an Even Number");
+		System.out.println("4 is an Even Number");
+	}
+    public void OddNumbers() {
+		
+		System.out.println("3 is an Even Number	");
 	}
 	
 }
