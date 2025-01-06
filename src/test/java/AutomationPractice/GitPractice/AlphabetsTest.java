c5bb507401e82b4ac9d06eb7b587596544a42835
Jaya <dhanipatijayalakshmi@gmail.com>
2025-01-06 18:09:19 +0530

    new commit

---
+++ src/test/java/AutomationPractice/GitPractice/AlphabetsTest.java
@@ -0,0 +1,15 @@
+package AutomationPractice.GitPractice;
+
+import org.testng.annotations.Test;
+
+public class AlphabetsTest {
+
+	@Test
+	public void Vowels() {
+		System.out.println("a is a Vowel");
+	}
+	@Test
+	public void consonants() {
+		System.out.println("B is a Consonant");
+	}
+}
--- src/test/java/AutomationPractice/GitPractice/NumbersTest.java
+++ src/test/java/AutomationPractice/GitPractice/NumbersTest.java
@@ -1,15 +1,21 @@
 package AutomationPractice.GitPractice;
 
+import org.testng.annotations.Test;
+
 public class NumbersTest {
 
+	@Test
 	public void EvenNumbers() {
 		
 		System.out.println("2 is an Even Number");
 		System.out.println("4 is an Even Number");
 	}
+	
+	@Test
     public void OddNumbers() {
 		
-		System.out.println("3 is an Even Number	");
+		System.out.println("3 is an Odd Number");
+		System.out.println("5 is an Odd Number");
 	}
 	
 }
